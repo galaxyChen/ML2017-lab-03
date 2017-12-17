5d458f68a86044304b9d3e11bfdf02ddb4d47f4e
galaxy <549278303@qq.com>
2017-12-17 23:46:37 +0800

    add comment

--- ensemble.py
+++ ensemble.py
@@ -48,24 +48,67 @@ class AdaBoostClassifier:
             zm = (w[m]*np.exp(-alpha[m]*y.A1*h)).sum()
             w[m+1] = w[m]/zm*np.exp(-alpha[m]*y.A1*h)
             print(" base classifier accuracy:%f"%((h==y.A1).sum()/len(h)))
+            print("")
             model_list.append(model)
             
         self.alpha = alpha
         self.model_list = model_list
 
-        return X,y
-            
+    def fitAndPredict(self,X,y,val_x,val_y):
+        # 这个是fit的升级版，能够输出每一轮基学习器学习完之后的准确率
+        print("")
+        print("begin to train")
+        # 样本数量
+        n = X.shape[0]
+        # 参数，初始化第一个参数为1/n
+        w = [np.ones(n)]*(self.M+1)
+        w[0] = w[0]/n
+        # 参数，初始化为0
+        alpha = [0]*(self.M+1)
+        # 保存每一轮的学习器
+        model_list = []
+        # 保存每一轮的测试集正确率
+        acc_history = []
+        for m in range(0,self.M):
+            # 初始化一个决策树，参数随便选的，默认参数会达到100%的训练正确率
+            model = self.model(max_depth=6,min_samples_split=10,min_samples_leaf=10,random_state=1010)
+            print("begin to learn the %d  base classifier"%(m+1))
+            # 训练模型
+            model.fit(X,y,sample_weight=w[m])
+            h = model.predict(X)
+            hm = h==y.A1
+            indicator = np.array([1 if not x else 0 for x in hm])
+            # indicator是课件中的指示函数
+            # em是课件中的Epsilon m
+            em = (w[m]*indicator).sum()
+            # 错误率大于0.5，随机结果都比这个好，抛弃训练器并停止训练
+            if em>0.5:
+                break
+            # 错误率0，停止训练
+            if em==0:
+                break
+            # 课件上面的更新公式
+            alpha[m] = 0.5*np.log((1-em)/em)
+            zm = (w[m]*np.exp(-alpha[m]*y.A1*h)).sum()
+            w[m+1] = w[m]/zm*np.exp(-alpha[m]*y.A1*h)
+            print("base classifier accuracy:%f"%((h==y.A1).sum()/len(h)))
+            # 保存模型
+            model_list.append(model)            
+            self.alpha = alpha
+            self.model_list = model_list
+            # 对测试集进行预测并保存正确率
+            pred=self.predict(val_x)
+            print("test set accuracy:")
+            acc = (pred==val_y.A1).sum()/len(pred)
+            print(acc)
+            acc_history.append(acc)
+            print("")
+
+        return acc_history
 
 
     def predict_scores(self, X):
-        '''Calculate the weighted sum score of the whole base classifiers for given samples.
-
-        Args:
-            X: An ndarray indicating the samples to be predicted, which shape should be (n_samples,n_features).
-
-        Returns:
-            An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
-        '''
+        # 这部分遍历每一个基学习器，分别得出一个预测结果，与对应的基学习器权重相乘并求和得到最后的预测结果
         h = np.zeros(X.shape[0])
         for i in range(len(self.model_list)):
             hi = self.model_list[i].predict(X)
@@ -73,17 +116,9 @@ class AdaBoostClassifier:
         return h
 
     def predict(self, X, threshold=0):
-        '''Predict the catagories for geven samples.
-
-        Args:
-            X: An ndarray indicating the samples to be predicted, which shape should be (n_samples,n_features).
-            threshold: The demarcation number of deviding the samples into two parts.
-
-        Returns:
-            An ndarray consists of predicted labels, which shape should be (n_samples,1).
-        '''
+        # 这部分先调用了predict_socre函数，返回的结果中大于阈值的标为正例
         h = self.predict_scores(X)
-        h = [1 if x>0 else -1 for x in h]
+        h = [1 if x>threshold else -1 for x in h]
         return h
         
 
--- train.py
+++ train.py
@@ -3,38 +3,62 @@ import extractFeature
 import numpy as np
 import ensemble as ada
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
 
 def load(filename):
     with open(filename, "rb") as f:
         return pickle.load(f)
+    
+def save(model, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(model, f)
 
 if __name__ == "__main__":
-    # extract feature and save features to file
+    # extractFeature是自己写的函数，在extractFeature文件中
     extractFeature.extract()
-    #load features
+    #读取上一步生成的特征文件
     print("loading features")
     face_data = np.matrix(load('./feature_face'))
     nonface_data = np.matrix(load('./feature_nonface'))
     
     print("divide training set and test set")
+    # 选取400个有脸和400个无脸作为训练集
     train_data = np.concatenate((face_data[0:400],nonface_data[0:400]),axis=0)
     y = np.matrix(([1]*400)+([-1]*400)).T
+    # 下面这几行是拿来打乱数据的，意义不大，可以删除
     train_data = np.concatenate((y,train_data),axis=1)
     np.random.shuffle(train_data)
     train_set = train_data[:,1:]
     train_y = train_data[:,0]
-    
+    # 最后的100个有脸和没脸的数据作为测试集
     test_data = np.concatenate((face_data[400:500],nonface_data[400:500]),axis=0)
     test_y = np.matrix(([1]*100)+([-1]*100)).T
+    # 下面这几行是拿来打乱数据的，意义不大，可以删除
     test_data = np.concatenate((test_y,test_data),axis=1)
     np.random.shuffle(test_data)
     test_set = test_data[:,1:]
     test_y = test_data[:,0]
-
+    # 开始训练
     print("ready to train")
+    # 基学习器是决策树
     model = DecisionTreeClassifier
-    AdaBoost=ada.AdaBoostClassifier(model,10)
-    AdaBoost.fit(train_set,train_y)
+    # 基学习器的数量
+    num_of_model = 10
+    # 声明一个adaboost并进行训练，同时预测测试集
+    AdaBoost=ada.AdaBoostClassifier(model,num_of_model)
+    acc_history=AdaBoost.fitAndPredict(train_set,train_y,test_set,test_y)
+    
+    # 最后测试测试集的正确率
     pred=AdaBoost.predict(test_set)
     print("test set accuracy:")
     print((pred==test_y.A1).sum()/len(pred))
+    # 生成题目需要的report.txt文件，这个文件要print出来才能看，直接看是很糟糕的排版
+    report = classification_report(test_y.A1, pred)
+    print(report)
+    save(report,'./report.txt')
+    # 打印准确率随着学习器数量变化的曲线
+    plt.plot(np.arange(num_of_model),acc_history,label="test acc")
+    plt.legend(loc=1)
+    plt.xlabel('number of base classifier')
+    plt.ylabel('test accuracy')
